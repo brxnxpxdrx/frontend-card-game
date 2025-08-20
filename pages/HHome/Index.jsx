e3cc117983dd6220eedce42547c632136283f5fd
brxnxpxdrx <newbrd3v@gmail.com>
2025-08-19 23:37:54 -0300

    corrigindo erro

--- pages/HHome/Index.jsx
+++ pages/HHome/Index.jsx
@@ -1,24 +1,31 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import api from '../../src/services/api'
 import './Style.css'
 
 function Home() {
+  const [users, setUsers] = useState([])
+
+  async function getUsers(){
+    const usersFromApi = await api.get('/usuarios')
+    setUsers(usersFromApi.data) // agora salva no estado
+    console.log(usersFromApi.data) // loga os users
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
 
   return (
     <div className='Container'>
-      <form >
-        <h1>
-          Cadastro de Usuários
-        </h1>
+      <form>
+        <h1>Cadastro de Usuários</h1>
         <input type="text" name='nome' placeholder='Nome'  />
         <input type="email" name='email' placeholder='Email' />
         <input type="password" name='senha' placeholder='Senha' />
-        <button>
-          Enviar
-        </button>
-        </form>
-  </div>
-
+        <button>Enviar</button>
+        <p>{JSON.stringify(users)}</p> {/* só pra mostrar */}
+      </form>
+    </div>
   )
 }
 

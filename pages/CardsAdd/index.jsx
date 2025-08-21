48d0e860f18c3f51cbae9205965d8aa077a64dde
brxnxpxdrx <newbrd3v@gmail.com>
2025-08-20 23:40:07 -0300

    tawind configurado

--- pages/CardsAdd/index.jsx
+++ pages/CardsAdd/index.jsx
@@ -5,6 +5,11 @@ const CardsAdd = () => {
         <div>
             <h1>Add a New Card</h1>
             {/* Add your form or card creation UI here */}
+            <h1 className="text-3xl text-red-100 font-bold underline">
+                <p class="text-blue-600 dark:text-sky-400">The quick brown fox...</p>
+
+    Hello world!
+  </h1>
         </div>
     );
 };

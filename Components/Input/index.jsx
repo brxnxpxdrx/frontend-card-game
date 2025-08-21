d0c637a36fcaf6fdb1258a807f6919d56c6fe9a5
brxnxpxdrx <newbrd3v@gmail.com>
2025-08-21 13:52:49 -0300

    campo de adicao dos cards

--- Components/Input/index.jsx
+++ Components/Input/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Input = ({ value, onChange, placeholder, type = 'text', ...props }) => {
     return (
-                 <input id="first-name" type="text" name="first-name" autocomplete="given-name" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                 <input  placeholder={props.legenda}  onChange={onChange} id="first-name" type="text" name="first-name" autocomplete="given-name" className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
 
     );
 };
--- pages/CardsAdd/index.jsx
+++ pages/CardsAdd/index.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../../Components/Input";
 import Button from "../../Components/Button";
 
 const CardsAdd = () => {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState(''); 
+    const [tipo, setTipo] = useState('');
+    const [atk, setAtk] = useState('');         
+    const [def, setDef] = useState('');
+    const  [cards, setCards] = useState([])
+
+    const handlerSubmit = (e) => {
+        e.preventDefault(); 
+       
+       
+          setCards( {name, description, image, tipo, atk, def} );
+          console.log(cards)
+    }
+    console.log(name)
+    console.log(description)
     return (
         <div className="bg-sky-500/50 h-96">
             
@@ -11,12 +28,24 @@ const CardsAdd = () => {
 Adicionar cards
    
   </h1>
-  <Input />
-  <Input />
-  <Input />
-  <Input />
-  <Input />
-  <Button />
+ 
+  <Input  legenda="nome" onChange={e => setName(e.target.value)}/>
+  <Input  legenda="Descrição" onChange={e=> setDescription(e.target.value)} />
+  <Input  legenda="imagen(url)" onChange={e => setImage(e.target.value)} />
+  <Input  legenda="tipo" onChange={e => setTipo(e.target.value)} />
+  <Input  legenda="atk" onChange={e => setAtk(e.target.value)}/>
+  <Input legenda="def" onChange={e => setDef(e.target.value)} />
+
+  <Button onClick={handlerSubmit}></Button>
+
+  <p>
+    {cards.name} 
+    {cards.description} 
+    {cards.image} 
+    {cards.tipo}
+    {cards.atk}
+    {cards.def}  </p>
+
         </div>
     );
 };

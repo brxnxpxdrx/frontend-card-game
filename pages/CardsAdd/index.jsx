f66acd1a489d8f30dc2d5a718002b194bf2da249
brxnxpxdrx <newbrd3v@gmail.com>
2025-08-22 20:13:07 -0300

    adicao de img default na miniatura do card

--- pages/CardsAdd/index.jsx
+++ pages/CardsAdd/index.jsx
@@ -12,7 +12,7 @@ const CardsAdd = () => {
     const [atk, setAtk] = useState('');         
     const [def, setDef] = useState('');
     const  [cards, setCards] = useState([])
-    const [img, setImg] = useState('');
+    const [img, setImg] = useState('https://youtooz.com/cdn/shop/products/yugioh_darkmagician_characterai_es_1000x1000_min-8qxy.png?v=1684524635');
 
     const handlerSubmit = async (e) => {
         e.preventDefault(); 
